320a5d5a2e7e3a07a88afcf82a17b12e4e242626
Ruben Araiza <raraiza@nearsoft.com>
2018-10-19 21:58:37 -0500

    modified setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from setuptools import setup
 
 setup(
     name='kass',
-    version='0.1',
+    version='0.0.1',
     description='Python library to draw on a screen',
-    url='https://github.com/raraizan/processing_like',
+    url='https://github.com/raraizan/Kass',
     author='Ruben Araiza',
     author_email='raraiza@nearsoft.com',
-    # license='MIT',
     packages=['kass'],
     install_requires=[
         'pygame',
